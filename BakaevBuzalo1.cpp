140ce6e70ef919be0afecf55e5d8c5aeb80bd269
clackgot <clackgot@gmail.com>
2020-12-17 17:52:45 +0300

    Добавьте файлы проекта.

---
+++ BakaevBuzalo1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	cout << "Выполнил работу: Бакаев Александр\t\t\tГруппа: ФИТУ 2-5" << endl << endl;
+	
+	unsigned long long int a, b = 0;
+	bool founded = false;
+	cin >> a;
+	clock_t start = clock();
+	if (a % 2 == 0)
+	{
+		cout << "NO" << endl;
+		cout << "Т.к. a % 2 == 0" << endl;
+	}
+	else
+	{
+		for (unsigned long long int n = 1; n <= 11111111111111111111; n = n * 10 + 1)
+		{
+			if (n % a == 0)
+			{
+				cout << n << endl;
+				founded = true;
+			}
+			++b;
+		}
+		if (!founded)cout << "NO" << endl;
+	}
+	clock_t end = clock();
+	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("Время выполнения: %f сек.\n", seconds);
+	return 0;
+}
